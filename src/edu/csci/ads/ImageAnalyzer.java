832fa913143a05d2cb2b8131c73ce75097a320e8
wdp2012 <wdp2020@163.com>
2016-12-05 18:16:51 -0800

    Add logo detector

--- src/edu/csci/ads/ImageAnalyzer.java
+++ src/edu/csci/ads/ImageAnalyzer.java
@@ -6,11 +6,18 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayDeque;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Vector;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import edu.csci.playVideo.VideoLoader;
 
@@ -21,13 +28,16 @@ public class ImageAnalyzer {
 	public int colorNumber = 5;
 	public int numThreshold = 1000;
 	public int colorThreshold = 30;
-	BufferedImage img;
-    
-	public ImageAnalyzer(BufferedImage img){
-		this.img = img;
-	}
-    
-	public Vector<Integer> getDominateColors(){
+	public int defaultColor = Color.GRAY.getRGB();
+	public int frameDisThreshold = 20;
+	public int logoDisThreshold = 40;
+	public int blockThreshold = 20;
+	public int blockSizeThreshold = 400;
+	private int[] dx = {0, 0, 1, -1};
+	private int[] dy = {-1, 1, 0, 0};
+	JFrame frame = new JFrame("blocks");
+
+	public Vector<Integer> getDominateColors(BufferedImage img){
 		
 		HashMap<Integer, Integer> colorCount = new HashMap<Integer, Integer>();
 		
@@ -79,6 +89,198 @@ public class ImageAnalyzer {
 		return colors;
 		
 	}
+	
+	public BufferedImage getFilteredImage(BufferedImage oldImage, Vector<Integer> colors){
+		
+		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		
+    	for(int i = 0; i < width; i++){
+    		for(int j = 0; j < height; j++){
+    			Color c = new Color(oldImage.getRGB(i, j));
+    			
+    			int similarIndex = -1;
+    			Integer minDis = 10000000;
+    			for(int index = 0; index < colors.size(); index++){
+    				int rgb = colors.get(index);
+    				
+    				if(caculateColorDis(c, new Color(rgb)) < minDis){
+    					minDis = caculateColorDis(c, new Color(rgb));
+    					similarIndex = index;
+    				}
+    			}
+    			
+
+    			if(minDis < colorThreshold * 2){
+    				img.setRGB(i, j, colors.get(similarIndex));
+    			}else{
+    				img.setRGB(i, j, defaultColor);
+    			}
+    			
+    		}
+    	}
+		return img;
+	}
+	
+	public double calculateScore(BufferedImage frame, BufferedImage logo) throws InterruptedException{
+		
+		boolean visited[][] = new boolean[width][height];
+		
+		for(int i = 0; i < width; i++){
+			for(int j = 0; j < height; j++){
+				visited[i][j] = false;
+			}
+		}
+		double maxScore = 0.0;
+		for(int i = 0; i < width; i++){
+			for(int j = 0; j < height; j++)
+			if(frame.getRGB(i, j) != defaultColor && visited[i][j] == false){
+				visited[i][j] = true;
+				//System.out.println(i + " " + " " + j + " " + frame.getRGB(i, j));
+				Vector<Integer> loc = bfs(i, j, frame.getRGB(i, j), frameDisThreshold, frame, visited);
+				loc.add(frame.getRGB(i, j));
+				
+				if(preFilter(loc)){
+					continue;
+				}
+				
+				boolean logoVisited[][] = new boolean[width][height];
+				for(int x = 0; x < width; x++){
+					for(int y = 0; y < height; y++ ){
+						logoVisited[x][y] = false;
+					}
+				}
+				
+				BufferedImage block = frame.getSubimage(loc.get(1), loc.get(2), loc.get(3), loc.get(4));
+				//System.out.println(loc);
+				for(int x = 0; x < width; x++){
+					for(int y = 0; y < height; y++)
+					if(logo.getRGB(x, y) == frame.getRGB(i, j) && logoVisited[i][j] == false){
+						
+						Vector<Integer> logoLoc = bfs(x, y, frame.getRGB(i, j), logoDisThreshold, logo, logoVisited);
+						logoLoc.add(logo.getRGB(x, y));
+						
+						if(preFilter(logoLoc)){
+							continue;
+						}
+						
+						BufferedImage logoBlock = logo.getSubimage(logoLoc.get(1), logoLoc.get(2), logoLoc.get(3), logoLoc.get(4));
+						
+						double score = 0.0;
+						for(int ii = 0; ii < block.getWidth(); ii++){
+							for(int jj = 0; jj < block.getHeight(); jj++){
+								int rgb = block.getRGB(ii, jj);
+								int targetX = (int) (ii * 1.0 / block.getWidth() * logoBlock.getWidth());
+								int targetY = (int) (jj * 1.0 / block.getHeight() * logoBlock.getHeight());
+								targetX = Math.min(targetX, logoBlock.getWidth() - 1);
+								targetY = Math.min(targetY, logoBlock.getHeight() - 1);
+								int logoRgb = logoBlock.getRGB(targetX, targetY);
+								
+								score = score + calculateScore(rgb, logoRgb, frame.getRGB(i, j));
+								
+							}
+						}
+						
+						//for debug
+						show(block, logoBlock);
+						
+						score = score / (block.getWidth() * block.getHeight());
+						maxScore = Math.max(score, maxScore);
+					}
+				}
+				
+			}
+		}
+		System.out.println(maxScore);
+		return maxScore;
+	}
+	
+	private boolean preFilter(Vector<Integer> loc) {
+		if(loc.get(0) < blockThreshold){
+			return true;
+		}
+		
+		double lenX = loc.get(3);
+		double lenY = loc.get(4);
+		if( (lenX / lenY > 8) || (lenY / lenX > 8) || lenX * lenY < blockSizeThreshold){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private double calculateScore(int rgb, int logoRgb, int frameRgb){
+		if(rgb == frameRgb){
+			if(rgb == logoRgb){
+				return 2;
+			}else{
+				return 0;
+			}
+		}else{
+			if(rgb == logoRgb){
+				return 3;
+			}else{
+				if(logoRgb == frameRgb){
+					return 0;
+				}else{
+					return 1;
+				}
+			}
+		}
+	}
+
+	private Vector<Integer> bfs(int x, int y, int rgb, int length, BufferedImage img, boolean visited[][]){
+		
+		int minX = x, minY = y, maxX = x, maxY = y, count = 0;
+		Queue<Integer> q = new ArrayDeque<Integer>();
+		Vector<Integer> ret = new Vector<Integer>();
+		q.add(hashXY(x,y));
+		
+		while(q.isEmpty() == false){
+			x = q.remove();
+			y = x % width;
+			x = x / width;
+			count++;
+			for(int i = 0; i < length; i++){
+				for(int j = -length; j <= length; j++){
+					int newX = x + i, newY = y + j;
+					
+					if(newX >=0 && newX < width && newY >=0 && newY < height && visited[newX][newY] ==false && img.getRGB(newX, newY) == rgb){
+						visited[newX][newY] = true;
+						minX = Math.min(minX, newX);
+						minY = Math.min(minY, newY);
+						maxX = Math.max(maxX, newX);
+						maxY = Math.max(maxY, newY);
+						q.add(hashXY(newX,newY));
+					}
+					
+				}
+			}
+			
+		}
+		ret.add(count);
+		ret.add(minX);
+		ret.add(minY);
+		ret.add(maxX - minX + 1);
+		ret.add(maxY - minY + 1);
+		return ret;
+	}
+	
+	private int hashXY(int x, int y){
+		return x * width + y;
+	}
+	
+	public void show(BufferedImage img1, BufferedImage img2){
+		JPanel  panel = new JPanel ();
+	    panel.add (new JLabel (new ImageIcon (img1)));
+	    panel.add (new JLabel (new ImageIcon (img2)));
+
+		frame.getContentPane().removeAll();
+	    frame.getContentPane().add (panel);
+	    frame.pack();
+	    frame.setVisible(true);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	}
+	
 	
 	public int caculateColorDis(Color a, Color b){
 		return Math.abs(a.getRed() - b.getRed()) +
--- src/edu/csci/ads/LogoCMP.java
+++ src/edu/csci/ads/LogoCMP.java
@@ -20,19 +20,21 @@ public class LogoCMP {
 	int width = 480;
 	int height = 270;
 	int frameCount = 0;
-	int frequency = 2;
+	int freq = 15;
 	int fps = 30;
 	int logoCache[][];
 	int frameCache[][];
+	double scoreThreshold = 1.3;
+	double ratioThreshold = 1.4;
 	JFrame frame = new JFrame("display");
 	
-	public void findLogoInVideo(String logoPath, String videoPath) throws FileNotFoundException{
-    	VideoLoader loader = new VideoLoader();
+	public Vector<Integer> findLogoInVideo(String logoPath, String videoPath) throws FileNotFoundException, InterruptedException{
+    	VideoLoader logoLoader = new VideoLoader();
     	InputStream is = new FileInputStream(logoPath);
-    	BufferedImage logo = loader.getFrame(is, width, height);
-    	
-    	ImageAnalyzer analyzer = new ImageAnalyzer(logo);
-    	Vector<Integer> colors = analyzer.getDominateColors();
+    	BufferedImage logo = logoLoader.getFrame(is, width, height);
+
+    	ImageAnalyzer analyzer = new ImageAnalyzer();
+    	Vector<Integer> colors = analyzer.getDominateColors(logo);
     	
     	logoCache = new int[height][width];
     	frameCache = new int[height][width];
@@ -43,86 +45,77 @@ public class LogoCMP {
     		}
     	}
     	
+    	logo = analyzer.getFilteredImage(logo, colors);
     	is = new FileInputStream(videoPath);
-    	loader.notLastFrame = true;
-		
-    	
+    	VideoLoader loader = new VideoLoader();
+    	Vector<Double> scores = new Vector<Double>();
     	while(loader.notLastFrame){
     		
     		frameCount++;
     		BufferedImage thisFrame = loader.getFrame(is, width, height);
-    		
-    		BufferedImage oldFrame = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    		
+
+    		BufferedImage newFrame = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    		BufferedImage edgeFrame = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
     		
     		//skip frame
-//    		if(frameCount < 170 * 30){
+//    		if(frameCount < 290 * 30){
 //    			continue;
 //    		}
+
+
     		
-    		if(frameCount % Math.max((fps / frequency), 1) != 0){
+    		if(frameCount % freq != 0){
     			continue;
     		}
+    		
 
     		System.out.printf("%.4f : ", frameCount / 30.0);
-    		
-    		Vector<Integer> colorCount = new Vector<Integer>();
-    		for(int i = 0; i < colors.size(); i++){
-    			colorCount.add(0);
-    		}
-    		
-        	for(int i = 0; i < width; i++){
-        		for(int j = 0; j < height; j++){
-        			Color c = new Color(thisFrame.getRGB(i, j));
-        			oldFrame.setRGB(i, j, c.getRGB());
-        			int similarIndex = -1;
-        			Integer minDis = 10000000;
-        			for(int index = 0; index < colors.size(); index++){
-        				int rgb = colors.get(index);
-        				
-        				if(analyzer.caculateColorDis(c, new Color(rgb)) < minDis){
-        					minDis = analyzer.caculateColorDis(c, new Color(rgb));
-        					similarIndex = index;
-        				}
-        			}
-        			
-
-        			if(minDis < analyzer.colorThreshold * 2){
-        				thisFrame.setRGB(i, j, colors.get(similarIndex));
-        				colorCount.set(similarIndex, colorCount.get(similarIndex) + 1);
-        			}else{
-        				thisFrame.setRGB(i, j, Color.gray.getRGB());
-        			}
-        			
-        		}
-        	}
-        	
-
-    		show(oldFrame, thisFrame);
-        	
-    		//keep img for a while
-        	long start = System.currentTimeMillis();
-        	while(System.currentTimeMillis() - start < 1500){
-        		
-        	}
-        	
-        	int minValue = 100000000;
-        	for(Integer count : colorCount){
-        		minValue = Math.min(minValue, count);
-        	}
-        	
-        	System.out.println(colorCount + " " + minValue);
-        	
-        	
-        	
+    		newFrame = analyzer.getFilteredImage(thisFrame, colors);
+    		//for debug
+    		show(thisFrame,newFrame);
+    		scores.add(analyzer.calculateScore(newFrame, logo));
     	}
     	
+    	System.out.println(scores);
+    	return getAdsFrames(scores);
 	}
 	
-	public void show(BufferedImage logo, BufferedImage thisFrame){
+	public Vector<Integer> getAdsFrames(Vector<Double> scores){
+		
+		Vector<Integer> adsFrame = new Vector<Integer>();
+		
+    	double sum = 0.0;
+    	int count = 0;
+    	for(int i = 0; i < scores.size(); i++){
+    		if(scores.get(i) > 0.1){
+    			count++;
+    			sum = sum + scores.get(i);
+    		}
+    	}
+    	double avg = sum / count;
+		
+    	int lastAdsFrame = 0;
+    	
+    	for(int i = 0; i < scores.size(); i++){
+    		if(i - lastAdsFrame <= 20){
+    			continue;
+    		}
+    		
+    		if(scores.get(i) > scoreThreshold && scores.get(i) / avg > ratioThreshold ){
+    			adsFrame.add(freq * (i+1));
+    			lastAdsFrame = i;
+    		}
+    	}
+    	
+    	System.out.println(adsFrame);
+		return adsFrame;
+		
+	}
+	
+	public void show(BufferedImage img1, BufferedImage img2){
 		JPanel  panel = new JPanel ();
-	    panel.add (new JLabel (new ImageIcon (logo)));
-	    panel.add (new JLabel (new ImageIcon (thisFrame)));
+	    panel.add (new JLabel (new ImageIcon (img1)));
+	    panel.add (new JLabel (new ImageIcon (img2)));
 	    
 	    frame.getContentPane().add (panel);
 	    frame.pack();
@@ -130,9 +123,10 @@ public class LogoCMP {
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 	}
 	
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
 		LogoCMP cmp = new LogoCMP();
-		cmp.findLogoInVideo("src/data/starbucks_logo.rgb", "src/data/data_test1.rgb");
+		cmp.findLogoInVideo("src/data/subway_logo.rgb", "src/data/data_test1.rgb");
+
 	}
 
 }
